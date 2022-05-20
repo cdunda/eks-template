ceddd218c34113b82621126667c82bd594868cb3
Charles Dunda <s33dunda@gmail.com>
2022-05-20 15:32:33 -0400

    [eks-cluster-with-new-vpc] Mock the aws provider

--- eks-cluster-with-new-vpc/main.tf
+++ eks-cluster-with-new-vpc/main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-  region = local.region
+  region                      = local.region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 provider "kubernetes" {
